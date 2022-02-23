df43d60aef074d910c0fdc0304f4fd1ab1b1b0c1
Andy Holmes <beawareherenow@gmail.com>
2022-02-23 13:15:30 -0500

    initial

---
+++ lesson2_small_programs/calculator.rb
@@ -0,0 +1,5 @@
+# () ask the user for two numbers
+# () ask the user what type of operation to perform
+# addition, subtraction, multiplication, division
+# () perform the operation on the given numbers
+# () output the result
