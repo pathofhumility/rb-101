414c891189503a6f249b6da8cfdff2259325e257
Andy Holmes <beawareherenow@gmail.com>
2022-02-23 13:45:38 -0500

    perform  operator

--- lesson2_small_programs/calculator.rb
+++ lesson2_small_programs/calculator.rb
@@ -1,8 +1,8 @@
 # (x) ask the user for two numbers
-# () ask the user what type of operation to perform
+# (x) ask the user what type of operation to perform
 # addition, subtraction, multiplication, division
-# () perform the operation on the given numbers
-# () output the result
+# (x) perform the operation on the given numbers
+# (x) output the result
 
 Kernel.puts('Simple Calculator')
 Kernel.puts('')
@@ -12,15 +12,22 @@ Kernel.puts('Please enter second number:')
 number_2 = Kernel.gets.chomp()
 
 Kernel.puts('What operation do you wish to perform?')
-Kernel.puts('1)Addition 2)Subtraction 3)Multiplication 4)Division')
+Kernel.puts('1) Addition 2) Subtraction 3) Multiplication 4) Division')
 operation = Kernel.gets.chomp()
 
 case operation
-  when 1
-    result = number_1 + number_2
+  when '1'
+    result = number_1.to_i + number_2.to_i
     Kernel.puts("#{number_1} + #{number_2} = #{result}")
+  when '2'
+    result = number_1.to_i - number_2.to_i
+    Kernel.puts("#{number_1} - #{number_2} = #{result}")
+  when '3'
+    result = number_1.to_i * number_2.to_i
+    Kernel.puts("#{number_1} * #{number_2} = #{result}")
+  when '4'
+    result = number_1.to_f / number_2.to_f
+    Kernel.puts("#{number_1} / #{number_2} = #{result}")
   else
     Kernel.puts('I\'m unsure what to do!')
 end
-
-Kernel.puts(number_1, number_2)
