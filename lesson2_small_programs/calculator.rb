c82f48049e157faf7e9955049a9db16cd0c4c0d3
Andy Holmes <beawareherenow@gmail.com>
2022-02-24 10:12:28 -0500

    rubocop styling

--- lesson2_small_programs/calculator.rb
+++ lesson2_small_programs/calculator.rb
@@ -7,27 +7,27 @@
 Kernel.puts('Simple Calculator')
 Kernel.puts('')
 Kernel.puts('Please enter first number:')
-number_1 = Kernel.gets.chomp()
+number1 = Kernel.gets.chomp()
 Kernel.puts('Please enter second number:')
-number_2 = Kernel.gets.chomp()
+number2 = Kernel.gets.chomp()
 
 Kernel.puts('What operation do you wish to perform?')
 Kernel.puts('1) Addition 2) Subtraction 3) Multiplication 4) Division')
 operation = Kernel.gets.chomp()
 
 case operation
-  when '1'
-    result = number_1.to_i + number_2.to_i
-    Kernel.puts("#{number_1} + #{number_2} = #{result}")
-  when '2'
-    result = number_1.to_i - number_2.to_i
-    Kernel.puts("#{number_1} - #{number_2} = #{result}")
-  when '3'
-    result = number_1.to_i * number_2.to_i
-    Kernel.puts("#{number_1} * #{number_2} = #{result}")
-  when '4'
-    result = number_1.to_f / number_2.to_f
-    Kernel.puts("#{number_1} / #{number_2} = #{result}")
-  else
-    Kernel.puts('I\'m unsure what to do!')
+when '1'
+  result = number1.to_i + number2.to_i
+  Kernel.puts("#{number1} + #{number2} = #{result}")
+when '2'
+  result = number1.to_i - number2.to_i
+  Kernel.puts("#{number1} - #{number2} = #{result}")
+when '3'
+  result = number1.to_i * number2.to_i
+  Kernel.puts("#{number1} * #{number2} = #{result}")
+when '4'
+  result = number1.to_f / number2.to_f
+  Kernel.puts("#{number1} / #{number2} = #{result}")
+else
+  Kernel.puts('I\'m unsure what to do!')
 end
