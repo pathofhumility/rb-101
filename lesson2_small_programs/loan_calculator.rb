4f3234547c08ea887f86113a0ede95d0902262ca
Andy Holmes <beawareherenow@gmail.com>
2022-03-01 13:47:09 -0500

    updates

--- lesson2_small_programs/loan_calculator.rb
+++ lesson2_small_programs/loan_calculator.rb
@@ -1,8 +1,3 @@
-# code formula
-# M -> monthly payment
-# P -> loan amount
-# J -> monthly interest rate
-# N -> loan duration (months)
 require 'yaml'
 
 PROMPTS = YAML.load_file('loan_calc_messages.yml')
@@ -15,15 +10,34 @@ def prompt(message)
   print ">> #{message}"
 end
 
-headings(PROMPTS['heading'])
-prompt(PROMPTS['loan_amt'])
-loan_amount = gets.chomp.to_f
-prompt(PROMPTS['annual_interest_rate'])
-monthly_int_rate = gets.chomp.to_f / 1200
-prompt(PROMPTS['duration'])
-loan_duration = gets.chomp.to_i
+def validate_input(result)
+  result.empty? || result.to_f <= 0.0
+end
 
-monthly_payment =
-  loan_amount *
-  (monthly_int_rate / (1 - (1 + monthly_int_rate)**(-loan_duration)))
+def get_input
+  loop do
+    result = gets.chomp
+    if validate_input(result)
+      puts "That's invalid, you must enter a number greater than zero(0)."
+    else
+      return result
+    end
+  end
+end
+
+headings(PROMPTS['heading'])
+
+prompt(PROMPTS['loan_amt'])
+loan_amount = get_input().to_f
+
+prompt(PROMPTS['annual_interest_rate'])
+monthly_int_rate = get_input().to_f / 1200
+
+prompt(PROMPTS['duration'])
+loan_duration = get_input().to_i
+
+monthly_payment = loan_amount *
+                  (monthly_int_rate /
+                  (1 - (1 + monthly_int_rate)**(-loan_duration)))
+
 puts "Monthly Payment = $#{monthly_payment.round(2)}"
