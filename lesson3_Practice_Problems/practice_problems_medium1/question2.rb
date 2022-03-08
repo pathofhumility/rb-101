ba3c3a281403e545c315bbbf97dae0abce7964c1
Andy Holmes <beawareherenow@gmail.com>
2022-03-08 14:11:46 -0500

    initial

---
+++ lesson3_Practice_Problems/practice_problems_medium1/question2.rb
@@ -0,0 +1,4 @@
+# Convert the evaluation to a string.
+puts "the value of 40 + 2 is " + (40 + 2).to_s
+# String interpolation to evaluate expression within the string.
+puts "The value of 40 + 2 is #{40 + 2}"
