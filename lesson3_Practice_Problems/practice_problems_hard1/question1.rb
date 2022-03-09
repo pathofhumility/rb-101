881bd012f53de1cbc19397a4c76f9e1bd48869c3
Andy Holmes <beawareherenow@gmail.com>
2022-03-09 09:44:53 -0500

    initial

---
+++ lesson3_Practice_Problems/practice_problems_hard1/question1.rb
@@ -0,0 +1,13 @@
+if false
+  greeting = "hello world"
+end
+
+puts greeting.nil?
+
+# Why is `greeting` bound to `nil`?
+# The if conditional does not create any special scope, but when the code is
+# interpreted, `greeting` is declared and set to `nil` as part of
+# the if conditional. Because the if conditional will always evaluate to false,
+# `greeting` will not be set to "hello world", but it has been declared and
+# initialized when the code was initially interpreted and thus will not throw an
+# error when it is evaluated as an argument for `puts`.
