b52a194925a2d03fd59ff7ca373b9934cd2d8e32
Andy Holmes <beawareherenow@gmail.com>
2022-03-09 14:15:34 -0500

    initial

---
+++ lesson3_Practice_Problems/practice_problems_hard1/question4.rb
@@ -0,0 +1,15 @@
+def is_an_ip_number?(word)
+  (0..255).include?(word.to_i)
+end
+
+def dot_separated_ip_address?(input_string)
+  dot_separated_words = input_string.split(".")
+  return false unless dot_separated_words.length == 4
+  while dot_separated_words.size > 0
+    word = dot_separated_words.pop
+    return false unless is_an_ip_number?(word)
+  end
+  true
+end
+
+puts dot_separated_ip_address?('123.23.21.11')
